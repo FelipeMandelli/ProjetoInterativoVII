df92a04da95f83a8c396266d6d29042bf4591381
MurilloCostelini <murillo14costelini@gmail.com>
2024-06-04 18:16:20 -0300

    Update espLora.ino

--- esp/espLora.ino
+++ esp/espLora.ino
@@ -3,18 +3,17 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char* ssid = "SSID_nome_da_rede_wifi"; // SSID da rede WiFi
-const char* password = "senha_da_rede_wifi"; // Senha da rede WiFi
+const char* ssid = "SSID_nome_da_rede_wifi";
+const char* password = "senha_da_rede_wifi";
 
 void setup() {
   Serial.begin(9600);
-  LoRa.begin(433E6); // Inicia o LoRa na frequência 433 MHz
+  LoRa.begin(433E6);
   LoRa.setSpreadingFactor(7);
   LoRa.setSignalBandwidth(125E3);
   LoRa.setCodingRate4(5);
-  LoRa.setSyncWord(0xF3); // Palavra de sincronização
+  LoRa.setSyncWord(0xF3);
 
-  // Conecta ao WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -26,18 +25,17 @@ void setup() {
 void enviarDadosWeb(String dados) {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    http.begin("http://endereco_da_api.com/receber_dados"); // URL da API
+    http.begin("http://endereco_da_api.com/receber_dados");
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
     int httpResponseCode = http.POST("dados=" + dados);
 
-    if (httpResponseCode == 200) {
+    if (httpResponseCode > 0) {
       String response = http.getString();
       Serial.println("Dados enviados com sucesso: " + response);
     } else {
-      Serial.println("Erro ao enviar dados: " + String(httpResponseCode));
+      Serial.println("Falha na conexão: " + String(httpResponseCode));
     }
-
     http.end();
   } else {
     Serial.println("Erro: Não conectado ao WiFi");
@@ -51,10 +49,9 @@ void loop() {
     while (LoRa.available()) {
       leitura += (char)LoRa.read();
     }
-    Serial.print("Status: ");
+    Serial.print("Dados recebidos: ");
     Serial.println(leitura);
 
-    // Envia os dados para a web
     enviarDadosWeb(leitura);
   }
 }
